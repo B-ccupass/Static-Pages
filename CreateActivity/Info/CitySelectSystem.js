2921680df63df745e7462dce498f1406acb40c18
LunAhEric <luochen9913@gmail.com>
2022-04-11 17:37:54 +0800

    info 大修

---
+++ CreateActivity/Info/CitySelectSystem.js
@@ -0,0 +1,62 @@
+const TaiwanUrl = 'https://raw.githubusercontent.com/LunAhEric/FileStorage/main/TaiwanAddress_Simple.json'
+let cityArray = []
+let districtArray = []
+
+
+window.onload = function () {
+    getFetchData()
+    citySelect.onchange = function () {
+        const selectCity = citySelect.value
+        districtArray = [''].concat(cityArray.find(x => x.CityName == selectCity).AreaList.map(x => `${x.ZipCode}-${x.AreaName}`))
+        setDistrictSelect()
+        checkUI()
+    }
+    districtSelect.onchange = function () {
+        checkUI()
+    }
+}
+function checkUI() {
+    if (citySelect.value == '') {
+        districtSelect.setAttribute('disabled', 'true')
+        submitBtn.setAttribute('disabled', 'true')
+
+    } else {
+        districtSelect.removeAttribute('disabled')
+    }
+
+    if (districtSelect.value == '') {
+        submitBtn.setAttribute('disabled', 'true')
+    } else {
+        submitBtn.removeAttribute('disabled')
+    }
+}
+function getFetchData() {
+    fetch(TaiwanUrl)
+        .then(res => res.json())
+        .then(data => {
+            cityArray = [{ CityName: '', AreaList: [] }].concat(data)
+            districtArray = ['']
+            setCitySelect()
+            setDistrictSelect()
+            checkUI()
+        })
+        .catch(ex => { console.log(ex) })
+}
+function setCitySelect() {
+    citySelect.innerHTML = ''
+    cityArray.forEach(city => {
+        let option = document.createElement('option')
+        option.innerText = city.CityName == '' ? '--請選擇城市--' : city.CityName
+        option.value = city.CityName
+        citySelect.appendChild(option)
+    })
+}
+function setDistrictSelect() {
+    districtSelect.innerHTML = ''
+    districtArray.forEach(district => {
+        let option = document.createElement('option')
+        option.innerText = district == '' ? '--請選擇區域--' : district
+        option.value = district
+        districtSelect.appendChild(option)
+    })
+}
---
+++ CreateActivity/Info/imgSystem.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+    // let img = document.querySelector('.showImg')
+    // img.setAttribute('src', './img/image.png')
+}
+let actImg = document.getElementById('actImg')
+actImg.addEventListener("change", function (e) {
+    const file = this.files[0]
+    const fileReader = new FileReader();
+    fileReader.onload = function (e) {
+        let img = document.querySelector('.showImg')
+        img.setAttribute('src', e.target.result)
+    }
+    fileReader.readAsDataURL(file);
+})
